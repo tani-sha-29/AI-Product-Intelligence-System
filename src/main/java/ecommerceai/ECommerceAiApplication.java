6f01378c5204205863ca67b39db327d7d6699f57
Tanisha <tanishasuyal43@gmail.com>
2026-07-19 12:17:35 +0530

    Added swagger documentation

--- src/main/java/ecommerceai/ECommerceAiApplication.java
+++ src/main/java/ecommerceai/ECommerceAiApplication.java
@@ -1,9 +1,18 @@
 package ecommerceai;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Product API", version = "1.0", description = "API for managing products of ecom platform")
+        ,servers = @Server(
+        url = "http://localhost:8080",
+        description = "This app is deployed on Local server"
+))
 public class ECommerceAiApplication {
 
     public static void main(String[] args) {
--- src/main/java/ecommerceai/controller/ProductController.java
+++ src/main/java/ecommerceai/controller/ProductController.java
@@ -2,6 +2,8 @@ package ecommerceai.controller;
 
 import ecommerceai.entity.Product;
 import ecommerceai.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/product")
+@Tag(name="Product Controller", description = "APIs for managing products")
 public class ProductController {
 
     ProductService productService;
@@ -19,24 +22,27 @@ public class ProductController {
     }
 
     @GetMapping("/allProducts")
+    @Operation(summary = "Get all the products",description = "Returns all the products for your search")
     public ResponseEntity<List<Product>> getAllProducts(){
        List products= productService.getProducts();
-
        return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
     }
 
     @PostMapping("/addProduct")
+    @Operation(summary = "Add the product",description = "Add the Product from User to Database")
     ResponseEntity<String> addProduct(@RequestBody Product product){
         String response = productService.addProduct(product);
         return new ResponseEntity<String>(response,HttpStatus.CREATED);
     }
     @PutMapping("/updateProduct")
+    @Operation(summary = "Update the product",description = "Updates the fields")
     ResponseEntity<String> updateProduct(@RequestBody  Product product){
         String response=productService.updateProduct(product);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/deleteProduct/{id}")
+    @Operation(summary = "Deletes the product",description = "Delete the product by id")
     ResponseEntity<String> delete(@PathVariable Long id){
         String response=productService.deleteProduct(id);
         return new ResponseEntity<>(response, HttpStatus.OK);
